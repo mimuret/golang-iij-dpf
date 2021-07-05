9128b359223aae52430938c55b8080a78c129cd7
Manabu Sonoda <mimuret@gmail.com>
2021-07-05 18:57:56 +0900

    add Content-Type

--- pkg/api/client.go
+++ pkg/api/client.go
@@ -121,6 +121,7 @@ func (c *Client) Do(spec Spec, action Action, body interface{}, params SearchPar
 
 	// authorized
 	req.Header.Add("Authorization", "Bearer "+c.Token)
+	req.Header.Add("Content-Type", "application/json")
 	client := &http.Client{}
 
 	// request
